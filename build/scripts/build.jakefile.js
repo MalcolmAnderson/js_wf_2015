9a18159b1962de70a89546c01a4852ba20724c01
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2015-06-14 20:54:33 -0700

    Added placeholder lint tasks

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -5,11 +5,30 @@
 
     var startTime =Date.now();
 
-    task( "default", [ "version" ], function() {
+    desc( "Lint and test");
+    task( "default", [ "version", "lint" ], function() {
         var elapsedSeconds = (Date.now() - startTime) / 1000;
         console.log("\n\nBUILD OK (" + elapsedSeconds.toFixed(2) + "s)");
     });
 
+
+    //*** LINT
+
+    desc( "Lint everything");
+    task( "lint", [ "lintNode", "lintClient" ]);
+
+    task( "lintNode", function() {
+        console.log("Linting Node.js code: - TODO = add Code");
+    });
+
+    task( "lintClient", function() {
+        console.log("Linting browser code: - TODO = add Code");
+    });
+
+
+    //*** CHECK VERSION
+
+    desc("Check Node version");
     task("version", function() {
         console.log ("Check Node.js version: .");
         version.check({

cad6632a2472fd4d1541b671ed2157716a1c07ea
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2015-06-20 15:11:36 -0700

    Added command to start fire fox on build run

--- src/run.js
+++ src/run.js
@@ -12,4 +12,12 @@
     server.start(port, path.resolve(__dirname, "./client"), function() {
         console.log("Server started on port " + port);
     });
+
+    var sys = require('sys');
+    var exec = require('child_process').exec;
+    //function puts(error, stdout, stderr) { sys.puts(stdout) } exec("ls -la", puts);
+    function puts(error, stdout, stderr) { sys.puts(stdout) } exec("firefox http://localhost:5000/index.html", puts);
+
+    //firefox http://localhost:5000/index.html
+
 }());
